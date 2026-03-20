e736d771680bf4441179e5ce6724b8d446b0f066
joaquinmuzzi <joaquin.hola.muzzi@gmail.com>
2026-03-19 21:54:00 -0300

    Update polybar configuration and improve volume script logic

--- polybar/volume.sh
+++ polybar/volume.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2*100)}')
+RAW=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
 
-echo "VOL $VOL%"
+if echo "$RAW" | grep -q "MUTED"; then
+	echo "muted"
+else
+	VOL=$(echo "$RAW" | awk '{print int($2*100)}')
+	echo "$VOL%"
+fi
 

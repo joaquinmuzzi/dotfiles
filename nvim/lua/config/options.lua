53d4c85459b2f49dd2357e36e3c47f60148923b1
joaquinmuzzi <joaquin.hola.muzzi@gmail.com>
2026-03-19 22:57:36 -0300

    Update Alacritty configuration, add keymaps and options for Neovim, and enhance Polybar setup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.cursorline = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.pumblend = 10
+opt.winblend = 5
+opt.splitbelow = true
+opt.splitright = true
+opt.confirm = true
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,10 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+while pgrep -x polybar >/dev/null; do sleep 0.2; done
+
 # Launch bar1
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar -c "$HOME/.config/polybar/config.ini" bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."

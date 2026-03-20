32219cc40e3d190c3549562a9e5246d98d8da5ab
joaquinmuzzi <joaquin.hola.muzzi@gmail.com>
2026-03-19 21:36:08 -0300

    Convert nvim from submodule to normal folder

---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      opts.experimental = opts.experimental or {}
+      opts.experimental.ghost_text = false
+    end,
+  },
+}
